93595bc17f01093546cba1a9621469a8a94f4465
Sachit Shetty <sachit.shetty@gmail.com>
2024-12-21 21:11:14 -0500

    finish removing lambda call

--- src/api/MeetupApi.ts
+++ src/api/MeetupApi.ts
@@ -1,6 +1,30 @@
 import { GraphQLClient, gql } from 'graphql-request';
 import { AuthService } from './auth/AuthService';
 
+interface Group {
+  id: string;
+  name: string;
+  urlname: string;
+}
+
+interface PageInfo {
+  hasNextPage: boolean;
+  endCursor: string;
+}
+
+interface MembershipEdge {
+  node: Group;
+}
+
+interface MembershipResponse {
+  self: {
+    memberships: {
+      edges: MembershipEdge[];
+      pageInfo: PageInfo;
+    };
+  };
+}
+
 export class MeetupApi {
   private authService: AuthService;
 
@@ -8,23 +32,69 @@ export class MeetupApi {
 
   constructor () {
     this.authService = new AuthService();
-    this.gqlClient = new GraphQLClient('https://api.meetup.com/gql',
-      { headers: {} });
+    this.gqlClient = new GraphQLClient(
+      'https://api.meetup.com/gql',
+      { headers: {} }
+    );
 
     this.fetchGroups = this.fetchGroups.bind(this);
   }
 
-  async fetchGroups (searchText: string) {
-    const token = await this.authService.getValidToken();
+  async fetchGroups (searchText: string): Promise<Group[]> {
+    try {
+      const token = await this.authService.getValidToken();
 
-    if (!token) {
-      throw new Error('Not authenticated');
+      if (!token) {
+        throw new Error('Not authenticated');
+      }
+
+      this.gqlClient.setHeader('Authorization', `Bearer ${token}`);
+
+      const allGroups: Group[] = [];
+      let hasNextPage = true;
+      let afterCursor: string | null = null;
+
+      while (hasNextPage) {
+        const query: string = gql`
+          query {
+            self {
+              memberships(input: { first: 100${afterCursor ? `, after: "${afterCursor}"` : ''} }) {
+                edges {
+                  node {
+                    id
+                    name
+                    urlname
+                  }
+                }
+                pageInfo {
+                  hasNextPage
+                  endCursor
+                }
+              }
+            }
+          }
+        `;
+
+        // eslint-disable-next-line no-await-in-loop
+        const response = await this.gqlClient.request<MembershipResponse>(query);
+        const { edges, pageInfo } = response.self.memberships;
+
+        allGroups.push(...edges.map(edge => edge.node));
+
+        hasNextPage = pageInfo.hasNextPage;
+        afterCursor = pageInfo.endCursor;
+      }
+
+      // Filter groups if search text is provided
+      if (searchText) {
+        return allGroups.filter(group => group.name.toLowerCase().startsWith(searchText.toLowerCase()));
+      }
+
+      return allGroups;
+    } catch (error) {
+      console.error('Error fetching groups:', error);
+      throw error;
     }
-
-    const response = await fetch(`${import.meta.env.VITE_LAMBDA_API_URL}/get-user-groups?search=${searchText}`,
-      { headers: { Authorization: `Bearer ${token}` } });
-
-    return response.json();
   }
 
   async fetchGroupEvents (urlname: string) {
